1f8325244d924316d37684e99751a7473aa68223
TheNotoriousMac <damani.mccaskie@mycavehill.uwi.edu>
2022-02-07 22:37:45 -0400

    Finished Navbar. Now working on Hero Section.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact/>
+          <Route path='/' exact element={<Home />} />
         </Routes>
       </Router>
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import './Navbar.css';
 import { Button } from "./Button";
@@ -20,13 +20,17 @@ function Navbar() {
     }
   }
 
+  useEffect(() => {
+      showButton();
+  }, []);
+
   window.addEventListener('resize', showButton);
   
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             TRVL <i className="fab fa-typo3" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
