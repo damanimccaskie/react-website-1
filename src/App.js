e162a8bf28bca210ab9b2a749bf79b696423f0db
TheNotoriousMac <damani.mccaskie@mycavehill.uwi.edu>
2022-02-08 16:19:06 -0400

    Cards and Footer completed.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' exact element={<Home />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/sign-up' element={<SignUp />} />
         </Routes>
       </Router>
     </>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection';
 import Cards from '../Cards';
+import Footer from '../Footer';
 
 
 function Home() {
@@ -9,6 +10,7 @@ function Home() {
         <>
             <HeroSection />
             <Cards />
+            <Footer />
         </>
     )
 }
